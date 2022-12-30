f262cc6b03985e64def3baaebdf1899d6e95ce27
Z-HaiHua <121377355+Z-HaiHua@users.noreply.github.com>
2022-12-30 11:02:26 +0800

    pull test

--- src/main/java/com/zh/Hello.java
+++ src/main/java/com/zh/Hello.java
@@ -22,5 +22,13 @@ public class Hello {
         System.out.println("push test");
         System.out.println("push test");
         System.out.println("push test");
+        System.out.println("pull test");
+        System.out.println("pull test");
+        System.out.println("pull test");
+        System.out.println("pull test");
+        System.out.println("pull test");
+        System.out.println("pull test");
+        System.out.println("pull test");
+        System.out.println("pull test");
     }
 }
