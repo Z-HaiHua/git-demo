081df5a7fe528f5d791fde0b750ae2bb01af683c
Z-HaiHua <zhonghaihua0905@163.com>
2022-12-30 11:48:25 +0800

    DELETE

--- src/main/java/com/zh/Hello.java
+++ src/main/java/com/zh/Hello.java
@@ -3,11 +3,5 @@ package com.zh;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("push test");
-        System.out.println("push test");
-        System.out.println("push test");
-        System.out.println("push test");
-        System.out.println("push test");
-        System.out.println("push test");
-        System.out.println("push test");
     }
 }
