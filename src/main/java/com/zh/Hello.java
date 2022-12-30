64c341bdada9b215d9bcfefc69f566020ab95a03
Z-HaiHua <zhonghaihua0905@163.com>
2022-12-30 11:11:14 +0800

    PUSH TEST -SSH

--- src/main/java/com/zh/Hello.java
+++ src/main/java/com/zh/Hello.java
@@ -2,34 +2,9 @@ package com.zh;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("second");
-        System.out.println("我修改了master分支");
-        System.out.println("我创建了dev分支");
-        System.out.println("哈哈哈哈");
-        System.out.println("哈哈哈哈");
-        System.out.println("哈哈哈哈");
-        System.out.println("哈哈哈哈");
-        System.out.println("master");
-        System.out.println("dev");
-        System.out.println("我更新了代码");
-        System.out.println("push test");
-        System.out.println("pull test");
         System.out.println("push test");
         System.out.println("push test");
         System.out.println("push test");
         System.out.println("push test");
-        System.out.println("push test");
-        System.out.println("push test");
-        System.out.println("push test");
-        System.out.println("======================");
-        System.out.println("pull test");
-        System.out.println("pull test");
-        System.out.println("pull test");
-        System.out.println("pull test");
-        System.out.println("pull test");
-        System.out.println("pull test");
-        System.out.println("pull test");
-        System.out.println("pull test");
     }
 }
